307f8d992a1ff7472df75f9884b45a187cf7c6a2
bleutooth65 <51833105+bleutooth65@users.noreply.github.com>
2025-04-23 11:18:22 -0400

    2D Sweep Updates For Outputs And Inputs
    Sequence Helper Functions reworked to include looping; acquisitions longer than 65 us will now be split into uniform sections and looped to allow for longer acquisitions of the same voltage value. Similarly, setting the offset for the AWG with the QCM has been reworked in this fashion, allowing for voltage values to be played longer than 65 us. In general, not only do these updates generalize the code by allowing for longer pulses and acquisitions, the addition of looping reduces the number of commands sent to qblox for the same amount of steps in a given 2D sweep, allowing for 2D sweeps to contain more points in general.

---
+++ Libraries/QbloxSequenceHelpers/__init__.py
@@ -0,0 +1,10 @@
+'''
+Created on Thurs, April 17th, 2025
+
+Author: Ben Van Osch
+'''
+
+from .benchmarking import QbloxExperiment
+from .instrumentQblox import *
+from .predistort import *
+from .sequence_helperV2 import *
---
+++ Libraries/__init__.py
@@ -0,0 +1,9 @@
+'''
+Created on Thurs, April 17th, 2025
+
+Author: Ben Van Osch
+'''
+
+from . import LeCroyOscilloscope
+from . import QbloxSequenceHelpers
+from . import QbloxSequenceTemplates
